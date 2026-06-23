60d37f695a9781615e1dc3bc3121133b2671dcdf
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-06-23 21:58:26 +0600

    learned asConst and conditional type in typescript

---
+++ asConst.ts
@@ -0,0 +1,29 @@
+// as const assertion
+
+
+// type UserRoles = 'Admin' | 'Editor' | 'Viewer';
+
+// enum UserRoles2 {
+//     Admin = "Admin",
+//     Editor = "Editor",
+//     Viewer = "Viewer"
+// }
+
+// const canEdit = (role: UserRoles2) => {
+//     if(role === UserRoles2.Admin || role === UserRoles2.Editor) {
+//         return true
+//     } else return false
+// }
+
+// const isEditPermissable2 = canEdit(UserRoles2.Admin)
+// console.log(isEditPermissable2);
+
+
+// const akash = 'akash'
+
+
+const UserRoles2 = {
+    Admin : "Admin",
+    Editor : "Editor",
+    Viewer : "Viewer"
+}
